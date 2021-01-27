ee40e4696d187341e4c5b5587ecca20801539ee3
Pawan <achapawa@isu.edu>
2021-01-26 23:53:14 -0700

    Initial Commit

---
+++ src/main/java/edu/isu/cs/cs2263/App.java
@@ -0,0 +1,25 @@
+package edu.isu.cs.cs2263;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+
+public class App extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("My First JavaFX App");
+
+        Label label = new Label("Hello World, JavaFX !");
+        Scene scene = new Scene(label, 400, 200);
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module App.main{
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires jdk.internal.vm.compiler.management;
+    exports edu.isu.cs.cs2263;
+}
